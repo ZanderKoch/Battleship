bc5a0993a6fabd29e3b7ac34fdde6d6dda543780
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-03-23 12:00:16 +0100

    Create GameState.java

---
+++ battleship/src/main/java/internship/softwerk/battleship/GameState.java
@@ -0,0 +1,10 @@
+package internship.softwerk.battleship;
+
+/**
+ * singleton class responsible for the execution of commands parsed and sent by
+ * Commandparser
+ * @author Zander Koch
+ */
+public class GameState {
+    
+}
