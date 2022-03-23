066a29b1e455a460334efbd7c6bf7e7130d8a62b
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-03-23 14:15:55 +0100

    updated introduction text

--- battleship/src/main/java/internship/softwerk/battleship/Printer.java
+++ battleship/src/main/java/internship/softwerk/battleship/Printer.java
@@ -5,4 +5,8 @@ package internship.softwerk.battleship;
  * @author Zander Koch
  */
 public class Printer {
+    public static void printIntroduction(){
+        System.out.println("Welcome to Battleships!");
+        System.out.println("Type \"quit\" to quit at any time");
+    }
 }
