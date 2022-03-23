5bdf131e5a4e20ee942ba923b4b7617502a0c44f
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-03-23 16:21:23 +0100

    Implemented player constructor

---
+++ battleship/src/main/java/internship/softwerk/battleship/Player.java
@@ -0,0 +1,20 @@
+package internship.softwerk.battleship;
+
+import java.util.HashMap;
+
+/**
+ * contains and handles the mutation of a single player's data
+ * @author Zander Koch
+ */
+public class Player {
+    private HashMap<String,String> myGrid;
+    private HashMap<String,String> hitOrMissGrid;
+    
+    public Player(){
+        initializeGrids();
+    }
+    
+    private void initializeGrids(){
+        
+    }
+}
