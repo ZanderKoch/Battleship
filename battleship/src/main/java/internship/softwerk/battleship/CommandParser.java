64fa22a2950865a1c10536ac91e707a84b0920dd
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-03-24 10:41:28 +0100

    implemented validation of deploy and attack commands

--- battleship/src/main/java/internship/softwerk/battleship/CommandParser.java
+++ battleship/src/main/java/internship/softwerk/battleship/CommandParser.java
@@ -15,6 +15,8 @@ public class CommandParser {
         
         GameState gameState = GameState.getInstance();
         
+        //print deployment instructions
+        
         startParse();
         
     }
@@ -34,19 +36,51 @@ public class CommandParser {
                 return;
             }
             
-            /*check if input consits of a character, one or two numbers, and
-            optionally a space and a v or h*/
-            Pattern pattern = Pattern.compile(
-                    "^[a-j]\\d{1,2}$|^[a-j]\\d{1,2}\\s[vh]$"
-                    ,Pattern.CASE_INSENSITIVE);
-            Matcher matcher = pattern.matcher(input);
-            if (!matcher.matches()) {
-                System.out.println("Command was not recognised");
-                continue;
-            }
-            if (isDeploying) {
+            /*Refactoring?: maybe the contents of these two statements should be
+            methods*/
+            if(isDeploying){
+                //print deployment info (friendly board and available ships)
+                if (validateDeployCommand(input)) {
+                    //deploy a boat
+                } else {
+                    //print an error message
+                }
+            } else {
+                if (validateAttackCommand(input)) {
+                    //attack a square
+                } else {
+                    //print and error message
+                }
                 
             }
         }
+    }
+    
+    /**
+     * validates given command. Returns true for valid commands,
+     * false for invalid ones. Case insesitive
+     * @param command should consist of a character, one or two digits, and a 
+     * space followed by v or h. 
+     * @return 
+     */
+    private static boolean validateDeployCommand(String command){
+        Pattern pattern = Pattern.compile("^[a-j](?:[1-9]|10)\\s[vh]$"
+                ,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(command);
+        return matcher.matches();
+    }
+    
+    /**
+     * validates given command. Returns true for valid commands,
+     * false for invalid ones. Case insesitive
+     * @param command should consist of a character, followed a number 1-10,
+     * inclusive
+     * @return 
+     */
+    private static boolean validateAttackCommand(String command){
+        Pattern pattern = Pattern.compile("^[a-j](?:[1-9]|10)$"
+                ,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(command);
+        return matcher.matches();
     }
 }
