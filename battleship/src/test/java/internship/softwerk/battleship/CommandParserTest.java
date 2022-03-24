ac6fb8b1784df59c3d1f26043f59b2220cf4ff01
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-03-24 15:26:11 +0100

    added JUnit

---
+++ battleship/src/test/java/internship/softwerk/battleship/CommandParserTest.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package internship.softwerk.battleship;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Zander Koch
+ */
+public class CommandParserTest {
+    
+    public CommandParserTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class CommandParser.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        CommandParser.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    
+    @Test
+    public void testValidateDeployCommandlowercasePositive() {
+        System.out.println("validateDeployCommand()");
+        String input = "a6";
+        CommandParser.validateDeployCommand();
+    }
+    
+}
